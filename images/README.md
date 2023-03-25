805b89ff488c9e105b9c4de260eeb6dca545553e
ahmed-osama2022 <ahmadosama.2011@gmail.com>
2023-03-25 12:58:08 +0200

    Adding the tree file system for the first time...

---
+++ images/README.md
@@ -0,0 +1,3 @@
+# Photos folder
+
+## Put all your photos here!!, and the main photos here without a folder
---
+++ js/main.js
@@ -0,0 +1,47 @@
+/**
+ *  This was created by GhostWeb Design company
+ *  All rights reserved 2023.
+ */
+
+/**
+ * Start of global variables
+ */
+
+
+
+
+
+
+
+/**
+ * End of global variables
+ */
+
+
+/**
+ * Start helper fuctions
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * End helper fuctions
+ */
+
+
+/**
+ * End of script
+ */
