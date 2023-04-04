1aec35876d8daff715601e4968faee9622957898
AbdoFathy11 <abdofathy100100@gmail.com>
2023-04-04 04:03:38 +0200

    added menu-links

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@
 /**
  * Start of global variables
  */
-
+const burgerIcon = document.querySelector(".burger-icon")
+const linksMenu = document.querySelector(".links-menu")
 
 
 
@@ -21,7 +22,9 @@
 /**
  * Start helper fuctions
  */
-
+burgerIcon.addEventListener("click", () => {
+    (linksMenu.style.display === "none")? linksMenu.style.display = "flex" : linksMenu.style.display = "none"
+})
 
 
 
